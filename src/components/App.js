9776303ae02f79ca409c194693c1621e74c6e930
Ed McCormick <edward.mccormick3@gmail.com>
2022-12-22 21:07:55 -0500

    page up and running

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
-import { Button } from '@mui/material';
-import React from 'react';
+import { Button } from "@mui/material";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./HomePage";
 
 /**
  * This is the entry point for all of our react stuff
  */
 const App = () => {
-    return (
-        <div>
-            <h1>Welcome to dsfghjkl, Good luck!</h1>
-            <Button variant="contained">Hello World</Button>
-        </div>
-    );
+  return (
+    <div>
+      <h1>NHL Machine Learning</h1>
+      <Button variant="contained">Hello World</Button>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
